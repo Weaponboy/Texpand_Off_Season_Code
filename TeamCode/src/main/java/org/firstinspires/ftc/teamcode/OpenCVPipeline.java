06a456156120b5e9cc4448e8b1cf5aae3189910d
Weaponboy <Joshandy06@gmail.com>
2022-09-21 14:31:44 +0200

    Josh Commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVPipeline.java
@@ -6,9 +6,7 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Mat;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
-import org.openftc.easyopencv.OpenCvWebcam;
 
 public class OpenCVPipeline extends OpenCvPipeline {
 
@@ -19,8 +17,10 @@ public class OpenCVPipeline extends OpenCvPipeline {
 
     //Start streaming from camera
 
+
     @Override
     public Mat processFrame(Mat input) {
         return null;
     }
+
 }
